4893f6e93e69791453b3851c9653fa014940a2ae
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2017-03-15 09:36:35 +0100

    added sensors script

--- install.sh
+++ install.sh
@@ -4,8 +4,12 @@ ln -sf $BASE/server/ext/apc.sh /usr/lib/xymon/server/ext/apc.sh
 ln -sf $BASE/server/ext/pfsense.sh /usr/lib/xymon/server/ext/pfsense.sh
 ln -sf $BASE/server/ext/pfsense.pl /usr/lib/xymon/server/ext/pfsense.pl
 
+ln -sf $BASE/client/ext/sensors /usr/lib/xymon/client/ext/sensors
+
 ln -sf $BASE/tasks.d/apc.cfg /etc/xymon/tasks.d/apc.cfg
 ln -sf $BASE/tasks.d/pfsense.cfg /etc/xymon/tasks.d/pfsense.cfg
+
+ln -sf $BASE/clientlaunch.d/sensors.cfg /etc/xymon/clientlaunch.d/sensors.cfg
 
 ln -sf $BASE/xymonserver.d/apc.cfg /etc/xymon/xymonserver.d/apc.cfg
 
