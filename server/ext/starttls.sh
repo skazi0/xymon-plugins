f87204c4038cf9b438e2361d89ed27f96aa3f155
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2017-08-16 12:45:07 +0200

    added support for multiple starttls tests per host

--- server/ext/starttls.sh
+++ server/ext/starttls.sh
@@ -3,41 +3,45 @@
 REDDAYS=7
 YELLOWDAYS=14
 
-# To enable starttls tests, add the "starttls=<host>:<port>:<proto>" tag to hosts.cfg.
+# To enable starttls tests, add the "starttls=<host>:<port>:<proto>[,<host>:<port>:<proto>]" tag to hosts.cfg.
 $XYMONHOME/bin/xymongrep --noextras "starttls*" | while read ip host hash line; do
     param=$(echo "$line" | cut -d'=' -f2)
-    domain=$(echo "$param" | cut -d':' -f1)
-    port=$(echo "$param" | cut -d':' -f2)
-    proto=$(echo "$param" | cut -d':' -f3)
-
-    output=$(echo Q | openssl s_client -host $domain -port $port -starttls $proto 2>&1)
-    res=$?
 
     color='clear'
+    statusout=''
 
-    if [ $res -eq 0 ]; then
-        subject=$(echo "$output" | grep subject= | cut -d= -f2-)
-        issuer=$(echo "$output" | grep issuer= | cut -d= -f2-)
+    for stest in $(echo "$param" | tr ',' ' '); do
+        addr=$(echo "$stest" | cut -d':' -f1-2)
+        proto=$(echo "$stest" | cut -d':' -f3)
 
-        cert=$(echo "$output" | sed '/--BEGIN CERTIFICATE--/,/--END CERTIFICATE--/!d' | openssl x509 -noout -text)
-        startstr=$(echo "$cert" | grep 'Not Before:' | cut -d':' -f2-)
-        endstr=$(echo "$cert" | grep 'Not After :' | cut -d':' -f2-)
-        start=$(date -d "$startstr" +%s)
-        end=$(date -d "$endstr" +%s)
-        today=$(date +%s)
-        daysleft=$(( ($end-$today)/(24*60*60) ))
-        size=$(echo "$cert" | grep 'Public-Key:' | cut -d':' -f2)
+        output=$(echo Q | openssl s_client -connect $addr -starttls $proto 2>&1)
+        res=$?
 
-        if [[ $daysleft -le $REDDAYS ]]; then
-            color='red'
-        elif [[ $daysleft -le $YELLOWDAYS ]]; then
-            color='yellow'
-        else
-            color='green'
-        fi
+        scolor='clear'
 
-        output="
-&$color SSL certificate for $proto://$domain:$port expires in $daysleft days
+        if [ $res -eq 0 ]; then
+            subject=$(echo "$output" | grep subject= | cut -d= -f2-)
+            issuer=$(echo "$output" | grep issuer= | cut -d= -f2-)
+
+            cert=$(echo "$output" | sed '/--BEGIN CERTIFICATE--/,/--END CERTIFICATE--/!d' | openssl x509 -noout -text)
+            startstr=$(echo "$cert" | grep 'Not Before:' | cut -d':' -f2-)
+            endstr=$(echo "$cert" | grep 'Not After :' | cut -d':' -f2-)
+            start=$(date -d "$startstr" +%s)
+            end=$(date -d "$endstr" +%s)
+            today=$(date +%s)
+            daysleft=$(( ($end-$today)/(24*60*60) ))
+            size=$(echo "$cert" | grep 'Public-Key:' | cut -d':' -f2)
+
+            if [[ $daysleft -le $REDDAYS ]]; then
+                scolor='red'
+            elif [[ $daysleft -le $YELLOWDAYS ]]; then
+                scolor='yellow'
+            else
+                scolor='green'
+            fi
+
+            statusout="$statusout
+&$scolor SSL certificate for $proto://$addr expires in $daysleft days
 
 Server certificate:
         subject: $subject
@@ -45,14 +49,25 @@ Server certificate:
         end date: $(date -u -d @$end +"%F %T %Z")
         key size: $size
         issuer: $issuer
+
 "
-    else
-        color='red'
-    fi
+        else
+            scolor='red'
+            statusout="$statusout
+$output
+
+"
+        fi
+
+        # global color
+        if [[ $color == 'clear' || $scolor == 'red' || $color != 'red' && $scolor == 'yellow' ]]; then
+            color=$scolor
+        fi
+    done
 
     # send status
     ( echo "status $host.sslcert $color `date`"
       echo
-      echo "$output"
+      echo "$statusout"
     ) | bb $XYMSRV @
 done
