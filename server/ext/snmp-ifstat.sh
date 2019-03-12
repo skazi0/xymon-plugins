a923d7d55255fe612580d54907df25368ae744bf
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2019-03-12 12:41:51 +0100

    converted snmp-ifstat scripts to common format

--- server/ext/snmp-ifstat.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# To enable snmp host resources tests, add the "snmp-ifstat" tag to hosts.cfg.
-
-$XYMONHOME/bin/xymongrep --noextras "snmp-ifstat" | cut -d' ' -f1,2 | while read ip host; do
-    $XYMONHOME/ext/snmp-ifstat.pl $ip $host
-done
